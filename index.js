7e2a93c464fa2a496adeb8098715a634cc03a4bf
jys129 <jys129@gmail.com>
2024-05-10 01:42:18 +0000

    수정

--- index.js
+++ index.js
@@ -132,7 +132,3 @@ const h2 = new Cotton('h2', {
 })
 
 const cottonCursor = new Cotton('#cotton-cursor')
-
-// or using DOM element
-const cursor = document.querySelector('#cotton-cursor')
-const cottonCursor = new Cotton(cursor)
