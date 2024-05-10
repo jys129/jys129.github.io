5654d2c422866a7ebe7e470bca9c05a44d17d25e
jys129 <jys129@gmail.com>
2024-05-10 00:24:00 +0000

    수정

--- index.js
+++ index.js
@@ -116,3 +116,20 @@ function hideMail() {
 function getRandomInt(max) {
 	return Math.floor(Math.random() * max);
 }
+
+// init ball and set h1 & h2 as models (default class)
+const ball = new Cotton('#ball', {
+    speed: 1
+})
+
+// init h1 with airMode
+const h1 = new Cotton('h1', {
+    airMode: true
+})
+
+// init h2 with airMode reverse
+const h2 = new Cotton('h2', {
+    airMode: {
+        reverse: true
+    }
+})
